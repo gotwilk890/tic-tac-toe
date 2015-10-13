88429f798bc9a31bb2756b7e896ae992ce254c91
gotwilk890 <dave.wilk890@gmail.com>
2015-10-13 18:48:05 -0400

    inputed winning logic and tested with success

--- ttt.js
+++ ttt.js
@@ -1,25 +1,65 @@
 
-
+// var possibleWins = [7, 56, 73, 84, 146, 273, 292, 448];
 var turns = 0;
-var player = 'x'
-
+var player = "o"
+var gameArray = [];
+var winner;
 var playerTurn = function playerTurn(){
+  gameArray = [];
 
   if($(this).is(':empty')){
-    if(player === 'o'){
-      $(this).text('x');
+    if(player === "o"){
+      $(this).text("x");
       console.log("added x");
       turns++;
       console.log(turns);
-      player = 'x';
+      player = "x";
     }
     else{
-      $(this).text('o');
+      $(this).text("o");
       console.log("added o");
       turns++;
       console.log(turns);
-      player = 'o';
+      player = "o";
     }
+    $('#cells div').each(function(){
+      gameArray.push($(this).text());
+    });
+  }
+  console.log("There isa " + winnerIsX(player));
+};
+
+
+//gameArray:
+//[0][1][2]
+//[3][4][5]
+//[6][7][8]
+
+var allThreeX = function allThreeX(player, cell1, cell2, cell3){
+  return (cell1 === player) && (cell2 === player) && (cell3 === player);
+};
+
+var winsRowX = function winsRowX(player){
+  return allThreeX(player, gameArray[0], gameArray[1], gameArray[2]) ||
+         allThreeX(player, gameArray[3], gameArray[4], gameArray[5]) ||
+         allThreeX(player, gameArray[6], gameArray[7], gameArray[8]);
+};
+
+
+var winsColumnX = function winsColumnX(player) {
+  return allThreeX(player, gameArray[0], gameArray[3], gameArray[6]) ||
+         allThreeX(player, gameArray[1], gameArray[4], gameArray[7]) ||
+         allThreeX(player, gameArray[2], gameArray[5], gameArray[8]);
+};
+
+var winsDiagonalX = function winsDiagonalX(player) {
+  return allThreeX(player, gameArray[0], gameArray[4], gameArray[8]) ||
+         allThreeX(player, gameArray[2], gameArray[4], gameArray[6]);
+}
+
+var winnerIsX = function winnerIsX(player) {
+  if(winsRowX(player) || winsColumnX(player) || winsDiagonalX(player)){
+    return player;
   }
 };
 
