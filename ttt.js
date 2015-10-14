800d2c5c1bb49f4b31c9c4b7ff8e7efc504594f4
gotwilk890 <dave.wilk890@gmail.com>
2015-10-14 13:46:35 -0400

    Clear board button along with restarting the game when pressed.

--- ttt.js
+++ ttt.js
@@ -7,6 +7,7 @@ var winner;
 var playerTurn = function playerTurn(){
   gameArray = [];
 
+
   if($(this).is(':empty')){
     if(player === "o"){
       $(this).text("x");
@@ -26,12 +27,13 @@ var playerTurn = function playerTurn(){
       gameArray.push($(this).text());
     });
   }
-  // if(winnerIsX(player) || turns === 9){
+
+  // if(getWinner(player) || turns === 9){
   //   (clearBoard());
   // }
 
-
-  console.log("There is a " + winnerIsX(player));
+  winner = getWinner(player);
+  console.log("There is a " + getWinner(player));
 };
 
 
@@ -40,37 +42,37 @@ var playerTurn = function playerTurn(){
 //[3][4][5]
 //[6][7][8]
 
-var allThreeX = function allThreeX(player, cell1, cell2, cell3){
+var allThree = function allThree(player, cell1, cell2, cell3){
   return (cell1 === player) && (cell2 === player) && (cell3 === player);
 };
 
-var winsRowX = function winsRowX(player){
-  return allThreeX(player, gameArray[0], gameArray[1], gameArray[2]) ||
-         allThreeX(player, gameArray[3], gameArray[4], gameArray[5]) ||
-         allThreeX(player, gameArray[6], gameArray[7], gameArray[8]);
+var winsRow = function winsRow(player){
+  return allThree(player, gameArray[0], gameArray[1], gameArray[2]) ||
+         allThree(player, gameArray[3], gameArray[4], gameArray[5]) ||
+         allThree(player, gameArray[6], gameArray[7], gameArray[8]);
 };
 
 
-var winsColumnX = function winsColumnX(player) {
-  return allThreeX(player, gameArray[0], gameArray[3], gameArray[6]) ||
-         allThreeX(player, gameArray[1], gameArray[4], gameArray[7]) ||
-         allThreeX(player, gameArray[2], gameArray[5], gameArray[8]);
+var winsColumn = function winsColumn(player) {
+  return allThree(player, gameArray[0], gameArray[3], gameArray[6]) ||
+         allThree(player, gameArray[1], gameArray[4], gameArray[7]) ||
+         allThree(player, gameArray[2], gameArray[5], gameArray[8]);
 };
 
-var winsDiagonalX = function winsDiagonalX(player) {
-  return allThreeX(player, gameArray[0], gameArray[4], gameArray[8]) ||
-         allThreeX(player, gameArray[2], gameArray[4], gameArray[6]);
+var winsDiagonal = function winsDiagonal(player) {
+  return allThree(player, gameArray[0], gameArray[4], gameArray[8]) ||
+         allThree(player, gameArray[2], gameArray[4], gameArray[6]);
 }
 
-var winnerIsX = function winnerIsX(player) {
-  if(winsRowX(player) || winsColumnX(player) || winsDiagonalX(player)){
-    $(".title h2").text("The winner is " + player);
-
-    return player;
-  }
-  else if(turns === 9){
-    $(".title h2").text("Cats Game");
-  }
+var getWinner = function getWinner(player) {
+     if(winsRow(player) || winsColumn(player)  || winsDiagonal(player)){
+      $(".title h2").text("The winner is " +  player);
+      $("#cells div").css("pointer-events", "none");
+      return player;
+    }
+    else if(turns === 9){
+      $(".title h2").text("Cats Game");
+    }
 };
 
 var clearBoard = function clearBoard(){
@@ -79,10 +81,13 @@ var clearBoard = function clearBoard(){
   }
 
 
-
-$("#cells div").click(playerTurn);
-$("#cells").click(function(){
-  if(winnerIsX(player) || turns === 9){
-    clearBoard();
-  };
+$(document).ready(function function_name (argument) {
+  // body...
+  $("#cells div").click(playerTurn);
+  $("#clear").click(function(){
+    if(getWinner(player) || turns === 9){
+      clearBoard();
+    };
+    $("#cells div").css("pointer-events", "auto");
+  });
 });
